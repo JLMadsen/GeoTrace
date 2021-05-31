7792994ca932ffdec181d3ec93a853d77acfa5ae
Jakob <jakob.m@dsen.tv>
2021-05-31 12:38:39 +0200

    Add DrawLineWithArrow

--- utils.cpp
+++ utils.cpp
@@ -6,6 +6,7 @@
 #include <QProcess>
 #include <QDebug>
 #include <QObject>
+#include <QPainter>
 
 #include "node.cpp"
 
@@ -60,6 +61,28 @@ QStringList exec_traceroute(Node* target)
     return retval;
 }
 
+void DrawLineWithArrow(QPainter& painter, QPoint start, QPoint end) {
 
+  painter.setRenderHint(QPainter::Antialiasing, true);
+
+  qreal arrowSize = 20; // size of head
+  painter.setPen(Qt::green);
+  painter.setBrush(Qt::green);
+
+  QLineF line(end, start);
+
+  double angle = std::atan2(-line.dy(), line.dx());
+  QPointF arrowP1 = line.p1() + QPointF(sin(angle + M_PI / 3) * arrowSize,
+                                        cos(angle + M_PI / 3) * arrowSize);
+  QPointF arrowP2 = line.p1() + QPointF(sin(angle + M_PI - M_PI / 3) * arrowSize,
+                                        cos(angle + M_PI - M_PI / 3) * arrowSize);
+
+  QPolygonF arrowHead;
+  arrowHead.clear();
+  arrowHead << line.p1() << arrowP1 << arrowP2;
+  painter.drawLine(line);
+  painter.drawPolygon(arrowHead);
+
+}
 
 }
