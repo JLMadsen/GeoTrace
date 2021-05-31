16c148dc7dec4e6daf6b4cd1b447af814bd41166
Jakob <jakob.m@dsen.tv>
2021-05-31 13:32:00 +0200

    Set new font

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
     // check if env supports OpenSSL
     qDebug() << "OpenSSL support status:" << QSslSocket::supportsSsl();
     QApplication app (argc, argv);
+    QFont font("consolas");
+    font.setStyleHint(QFont::Monospace);
+    app.setFont(font);
     app.setWindowIcon(QIcon(":/images/favicon.ico"));
     MainWindow window;
     window.show();
